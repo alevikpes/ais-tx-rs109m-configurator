de4ac41cbf2884da1b88df6dab6866ba8983e795
alevikpes <3097359+alevikpes@users.noreply.github.com>
2026-07-24 15:30:22 +0200

    Some refactoring

--- src/configurator.py
+++ src/configurator.py
@@ -2,7 +2,7 @@
 
 class Configurator:
 
-    default_config = bytearray([
+    extended_config = bytearray([
         0x04, 0x2d, 0xd2, 0x7f, 0x06, 0x31, 0x30, 0x39,
         0x30, 0x34, 0x30, 0x31, 0x37, 0x33, 0x20, 0x20,
         0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
@@ -34,8 +34,8 @@ class Configurator:
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
         0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
-    ])
-    default_len = 0x40
+    ])  # Length of this is `0xf0` (240 bytes)
+    default_len = 0x40  # 64 bytes
     _config = None
 
     def __init__(self, config=None):
@@ -45,7 +45,7 @@ class Configurator:
         return f'[ 0x {self._config.hex('#').replace("#", ", 0x")} ]'
 
     @staticmethod
-    def _fromxbit(ba, x=6):
+    def fromxbit(ba, x = 6, digitalphaencoding = True):
         if ((x < 1) or (x > 7)):
             raise ValueError('BitLen must between 1 and 7')
 
@@ -63,22 +63,24 @@ class Configurator:
             remaining = shift + x - 8
             if remaining > 0:
                 b[i] |= (ba[pos + 1] << (8 -  shift)) & ((1 << x) -1)
-
-            if (b[i] & 0x20) != 0x20 and b[i] != 0:
+            if digitalphaencoding and (b[i] & 0x20) != 0x20 and b[i] != 0:
                 # not a digit, is alpha
                 b[i] = (b[i] & 0x1f) | 0x40
 
         return b
 
     @staticmethod
-    def _toxbit(ba: bytes, x: int=6):
+    def toxbit(ba, x = 6, digitalphaencoding = True):
         if ((x < 1) or (x > 7)):
             raise ValueError('BitLen must between 1 and 7')
 
         if len(ba) < 1:
             return [0xff, 0xff, 0xff, 0xff, 0xff, 0xff]
 
-        ba = bytearray(ba.decode().upper().encode('ascii'))
+        if digitalphaencoding:
+           ba = bytearray(
+               ba.encode('ascii', 'ignore').decode().upper().encode('ascii')
+           )
 
         n = len(ba) * x // 8
         if (len(ba) * x % 8) > 0:
@@ -100,102 +102,7 @@ class Configurator:
             # print(ba[i], " i=", i, " pos=", pos, " shift=", shift, " remaining=", remaining)
             if remaining > 0:
                 b[pos +1] |= (ba[i] >> (8 - shift))
-
         return b
-
-    def _get_uint(self, offset, length):
-        return int.from_bytes(self._config[offset:offset + length], 'little')
-
-    def _set_uint(self, offset, length, value):
-        self._config[offset:offset + length] \
-            = int(value).to_bytes(length, 'little')
-
-    def _get_bits(self, start_bit, length):
-        """Read a bitfield.
-
-        start_bit: bit offset from the beginning of the config.
-                   Bit 0 is the least-significant bit of byte 0.
-        length:    number of bits.
-        """
-        value = int.from_bytes(self._config, 'little')
-        mask = (1 << length) - 1
-        return (value >> start_bit) & mask
-
-    def _set_bits(self, start_bit, length, field_value):
-        """Write a bitfield.
-
-        start_bit: bit offset from the beginning of the config.
-        length:    number of bits.
-        """
-        field_value = int(field_value)
-        if not 0 <= field_value < (1 << length):
-            raise ValueError(
-                f'value must fit in {length} bits '
-                f'(0..{(1 << length) - 1})'
-            )
-
-        value = int.from_bytes(self._config, 'little')
-
-        mask = ((1 << length) - 1) << start_bit
-        value &= ~mask
-        value |= field_value << start_bit
-
-        self._config[:] = value.to_bytes(len(self._config), 'little')
-
-    def _get_byte(self, offset):
-        return self._config[offset]
-
-    def _set_byte(self, offset, value):
-        self._config[offset] = int(value) & 0xff
-
-    def _get_ascii(self, offset, length, encoding='ascii', strip=True):
-        s = bytes(self._config[offset:offset + length]).decode(encoding)
-        return s.rstrip(' \x00') if strip else s
-
-    def _set_ascii(
-        self,
-        offset,
-        length,
-        value,
-        encoding='ascii',
-        pad=' ',
-        uppercase=False
-    ):
-        value = str(value)
-        if uppercase:
-            value = value.upper()
-
-        data = (
-            value.encode(encoding, 'ignore')[:length]
-            .ljust(length, pad.encode(encoding))
-        )
-        self._config[offset:offset + length] = data
-
-    def _get_xbit(self, offset, length, width=6, reverse=False):
-        s = self._fromxbit(
-            self._config[offset:offset + length],
-            width,
-            True
-        ).decode('ascii')
-
-        if reverse:
-            s = s[::-1]
-
-        return s.rstrip(' @\x00')
-
-
-    def _set_xbit(self, offset, length, value, width=6, reverse=False):
-        value = ''.join(
-            c.upper()
-            for c in str(value)
-            if c.isalnum()
-        )
-
-        if reverse:
-            value = value[::-1]
-
-        encoded = self._toxbit(value, width, True)
-        self._config[offset:offset + length] = encoded[:length].ljust(length, b'\x00')
 
     ### Config ###
     @property
@@ -206,136 +113,225 @@ class Configurator:
     def config(self, config):
         # TODO: implement differently, as setting config as slices
         # config[34:38] might be convenient
-        if config is None:
-            # self._config = self.default_config[:self.default_len]
-            self._config = self.default_config.copy()
+        if config == []:
+            self._config = self.extended_config[:self.default_len]
         else:
-            clen = min(len(config), 0xff)
+            clen = 0xff if (len(config) > 0xff) else len(config)
             self._config = bytearray(
-                config[:clen] + self.default_config[clen:]
+                config[0:clen] + self.extended_config[clen:]
             )
 
     ### MMSI ###
     @property
     def mmsi(self):
-        return self._get_uint(1, 4)
+        mmsi = self._config[1] \
+            + (self._config[2] << 8) \
+            + (self._config[3] << 16) \
+            + ((self._config[4] & 0xff) << 24)
+        return mmsi
 
     @mmsi.setter
     def mmsi(self, mmsi):
-        self._set_uint(1, 4, mmsi)
+        mmsi = int(mmsi)
+        self._config[1] = mmsi & 0xff
+        self._config[2] = (mmsi >> 8) & 0xff
+        self._config[3] = (mmsi >> 16) & 0xff
+        self._config[4] = (mmsi >> 24) & 0xff
 
-    ### Ship Name ###
+    ### Ship name ###
     @property
-    def ship_name(self):
-        return self._get_ascii(5, 20)
+    def name(self):
+        name = bytes(self._config[5:25]).decode('ascii').strip()
+        return name
 
-    @ship_name.setter
-    def ship_name(self, name):
-        self._set_ascii(
-            5,
-            20,
-            name,
-            uppercase=True,
-            pad=' '
-        )
+    @name.setter
+    def name(self, name):
+        # TODO: check for invalid chars, this one is incomplete
+        safe_name = name \
+            .encode('ascii', 'ignore') \
+            .decode() \
+            .upper() \
+            .ljust(20, ' ') \
+            .encode('ascii')
+        self._config[5:25] = safe_name
 
     ### Interval ###
     @property
     def interval(self):
-        return self._get_byte(0) * 30
+        return self._config[0] * 30
 
     @interval.setter
     def interval(self, seconds):
-        seconds = max(30, min(600, int(seconds)))
-        self._set_byte(0, seconds // 30)
+        seconds = int(seconds)
+        if seconds > 600:
+            seconds = 600  # maximum 10 minutes
 
-    ### Ship Type ###
+        if seconds < 30:
+            seconds = 30  # minimum 30 seconds
+
+        self._config[0] = seconds // 30
+
+    ### Ship type ###
     @property
-    def ship_type(self):
-        return self._get_byte(31)
+    def shipncargo(self):
+        return int(self._config[31])
 
-    @ship_type.setter
-    def ship_type(self, shiptype):
-        self._set_byte(31, shiptype)
+    @shipncargo.setter
+    def shipncargo(self,shiptype):
+        self._config[31] = int(shiptype) & 0xff
 
     ### Vendor ID ###
     @property
-    def vendor_id(self):
-        chars = []
-        for start_bit in (29 * 8 + 4, 28 * 8 + 6, 28 * 8):
-            c = chr(self._get_bits(start_bit, 6) | 0x40)
-            if c.isalnum():
-                chars.append(c)
+    def vendorid(self):
+        vid = [
+            (self._config[29] >> 4) | ((self._config[30]  & 0x03) << 4) | 0x40,
+            (self._config[28] >> 6) | ((self.config[29] & 0x07) << 2) | 0x40,
+            (self._config[28] & 0x3f) | 0x40,
+        ]
+        return  ''.join(c if c.isalnum() else '' for c in bytes(vid).decode())
 
-        return ''.join(chars)
+    @vendorid.setter
+    def vendorid(self, vid):
+        # TODO: check for invalid chars, this one is incomplete
+        safe_vid = vid \
+            .encode('ascii', 'ignore') \
+            .decode() \
+            .upper() \
+            .ljust(3, '\x00')[:3] \
+            .encode('ascii')
+        #print(safe_vid)
+        self._config[28] = (safe_vid[2] & 0x3f) | ((safe_vid[1] << 6 ) & 0xff)
+        self._config[29] = ((safe_vid[1] >> 2) & 0x0f) | ((safe_vid[0] << 4) & 0xff)
+        self._config[30] = (self._config[30] & ~0x0f) | ((safe_vid[0] & 0x3f) >> 4)
 
-    @vendor_id.setter
-    def vendor_id(self, vid):
-        safe_vid = (
-            vid.encode('ascii', 'ignore')
-               .decode()
-               .upper()
-               .ljust(3, '\x00')[:3]
+    ### Unit model ###
+    @property
+    def unitmodel(self):
+        unitmodel = self._config[27] >> 4
+        return unitmodel
+
+    @unitmodel.setter
+    def unitmodel(self, unitmodel):
+        unitmodel = int(unitmodel)
+        if unitmodel < 0 or unitmodel > 15:
+            raise ValueError("UnitModel must be 0 < unitmodel <= 15")
+
+        self._config[27] = (
+            (self._config[27] & 0xf0) | ((int(unitmodel) & 0x0f) << 4)
         )
-        self._set_bits(29 * 8 + 4, 6, ord(safe_vid[0]) & 0x3f)
-        self._set_bits(28 * 8 + 6, 6, ord(safe_vid[1]) & 0x3f)
-        self._set_bits(28 * 8,     6, ord(safe_vid[2]) & 0x3f)
 
-    ### Unit Model ###
+    ### Serial number ###
     @property
-    def unit_model(self):
-        return self._get_bits(27 * 8 + 4, 4)
+    def sernum(self):
+        sernum = (
+            self._config[25] +
+            (self._config[26] << 8) +
+            ((self._config[27] & 0x0f) << 16)
+        )
+        return sernum
 
-    @unit_model.setter
-    def unit_model(self, unitmodel):
-        self._set_bits(27 * 8 + 4, 4, value)
+    @sernum.setter
+    def sernum(self, sernum):
+        sernum = int(sernum)
+        if sernum < 0 or sernum > ((1 << 20) -1):
+            raise ValueError('UnitSernum must be 0 <= sernum <=', ((1<<20)-1))
 
-    ### Serial Number ###
+        self._config[25] = sernum & 0xff
+        self._config[26] = (sernum >> 8) & 0xff
+        self._config[27] = (self._config[27] & 0xf0) | ((sernum >> 16) & 0x0f)
+
+    ### Call sign ###
     @property
-    def ser_num(self):
-        return self._get_bits(25 * 8, 20)
+    def callsign(self):
+        # TODO: check if it is 32:37 or 32:38
+        # derek@conniffe.com - changed to 32:38 to
+        # support valid 7 digit callsigns
+        s = fromxbit(self._config[32:38], 6, True).decode('ascii')[::-1]
+        return ''.join(c if c.isalnum() else '' for c in s)
 
-    @ser_num.setter
-    def ser_num(self, sernum):
-        self._set_bits(25 * 8, 20, value)
+    @callsign.setter
+    def callsign(self, cs):
+        safe_cs = ''.join(c if c.isalnum() else '' for c in cs)
+        # derek@conniffe.com - changed to 32:38 to
+        # support valid 7 digit callsigns
+        self._config[32:38] = toxbit(safe_cs[::-1])
 
-    ### Call Sign ###
+    @staticmethod
+    def _validate(value, bits, name):
+        value = int(value)
+        max_value = (1 << bits) - 1
+
+        if not 0 <= value <= max_value:
+            raise ValueError(f'{name} must be between 0 and {max_value}')
+
+        return value
+
+    ### AIS to bow ###
     @property
-    def call_sign(self):
-        return self._get_xbit(32, 6, reverse=True)
+    def refa(self):
+        return (self._config[39] >> 5) | ((self.config[38] & ((1 << 6) - 1)) << 3)
 
-    @call_sign.setter
-    def call_sign(self, value):
-        self._set_xbit(32, 6, value, reverse=True)
+    @refa.setter
+    def refa(self, a):
+        a = self._validate(a, 9, 'Reference a')
 
+        self._config[39] = (
+            (self._config[39] & ((1 << 5) - 1)) |
+            (((a & ((1 << 6) - 1)) << 5) & 0xff)
+        )
+        self._config[38] = (
+            (self._config[38] & ~((1 << 4) - 1)) |
+            ((a & ((1 << 9) - 1)) >> 3)
+        )
+
+    ### AIS to stern ###
     @property
-    def ais2bow(self):
-        return self._get_bits(38 * 8 + 3, 9)
+    def refb(self):
+        return (self._config[40] >> 4) | ((self._config[39] & ((1 << 5) - 1)) << 4)
 
-    @ais2bow.setter
-    def ais2bow(self, value):
-        self._set_bits(38 * 8 + 3, 9, value)
+    @refb.setter
+    def refb(self, b):
+        b = self._validate(b, 9, 'Reference b')
 
+        self._config[40] = (
+            (self._config[40] & ((1 << 4) - 1)) |
+            (((b & ((1 << 6) - 1)) << 4) & 0xff)
+        )
+        self._config[39] = (
+            (self._config[39] & ~((1 << 5) - 1)) |
+            ((b & ((1 << 9) - 1)) >> 4)
+        )
+
+    ### AIS to port ###
     @property
-    def ais2stern(self):
-        return self._get_bits(39 * 8, 9)
+    def refc(self):
+        return (self._config[41] >> 6) | ((self._config[40] & ((1 << 4) - 1)) << 2)
 
-    @ais2stern.setter
-    def ais2stern(self, value):
-        self._set_bits(39 * 8, 9, value)
+    @refc.setter
+    def refc(self, c):
+        c = self._validate(c, 6, 'Reference c')
 
+        self._config[41] = (
+            (self._config[41] & ((1 << 6) - 1)) |
+            (((c & ((1 << 6) - 1)) << 6) & 0xff)
+        )
+        self._config[40] = (
+            (self._config[40] & ~((1 << 4) - 1)) |
+            ((c & ((1 << 6) - 1)) >> 2)
+        )
+
+    ### AIS to starboard ###
     @property
-    def ais2port(self):
-        return self._get_bits(40 * 8 + 2, 6)
+    def refd(self):
+        return self._config[41] & ((1 << 6) - 1)
 
-    @ais2port.setter
-    def ais2port(self, c):
-        self._set_bits(40 * 8 + 2, 6, value)
+    @refd.setter
+    def refd(self, d):
+        d = self._validate(d, 6, 'Reference d')
 
-    @property
-    def ais2star(self):
-        return self._get_bits(41 * 8, 6)
+        self._config[41] = (
+            (self._config[41] & ~((1 << 6) - 1)) |
+            (d & ((1 << 6) - 1))
+        )
 
-    @ais2star.setter
-    def ais2star(self, d):
-        self._set_bits(41 * 8, 6, value)
+    def get_ais_position(self, field_name):
